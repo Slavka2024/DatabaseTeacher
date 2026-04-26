7aa3a223903985c338fa99cf481f16f153041355
SlavkaP <slavkap2005@narod.ru>
2026-04-26 08:50:04 +0300

    Добавлена процедура отчета по отделам

---
+++ DatabaseTeacher/sp_GetDepartmentReport.sql
@@ -0,0 +1,12 @@
+﻿CREATE OR ALTER PROCEDURE sp_GetDepartmentReport
+AS
+BEGIN
+    SELECT
+        d.name AS DepartmentName,
+        COUNT(e.id) AS EmployeeCount
+    FROM department d
+    LEFT JOIN employee e ON d.id = e.department_id
+    GROUP BY d.name
+    ORDER BY EmployeeCount DESC;
+END
+GO
